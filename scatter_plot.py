fcc0ff2d5e23ac2322e4c5afe7b6ed8c54c62a84
Abhigyan Ranjan <71014464+AbhigyanRanjan0505@users.noreply.github.com>
2021-03-23 20:28:08 +0530

    Add files via upload

---
+++ scatter_plot.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("covid.csv")
+fig = px.scatter(df, x="Date", y="Cases", color="Country",
+                 size_max=60)
+fig.show()
